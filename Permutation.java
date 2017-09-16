ca94437120a68ee8dfa56c6bb3071a8ab979a738
Akash <30862739+akashbadave7@users.noreply.github.com>
2017-09-16 18:18:57 +0530

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,21 +1,26 @@
 import java.util.*;
 class Permutation
-{
+{	
     	public static void main(String[] args)
     	{
+		// to read input from keyboard
 		Scanner s = new Scanner(System.in);
         	System.out.println("Enter the String:");
+		// to read a string 
 		String str = s.nextLine();
+		// to calculate length of string
        		int n = str.length();
+		// creating a object of class
         	Permutation permutation = new Permutation();
-        	permutation.permute(str, 0, n-1);
+		// calling permute function to find all permutation 
+        	permutation.permute(str, 0, n-1);// passing string,0 and lenght of string
     	}
 	public static void permute(String str, int l, int r)
-    	{
+    	{	//if string lenth is 1
         	if (l == r)
             		System.out.println(str);
         	else
-        	{
+        	{	
         		for (int i = l; i <= r; i++)
             		{
                 		str = swap(str,l,i);
@@ -24,6 +29,7 @@ class Permutation
             		}
         	}
     	}
+	// function to swap charactor in string
 	public static String swap(String a, int i, int j)
 	{
 		char temp;
